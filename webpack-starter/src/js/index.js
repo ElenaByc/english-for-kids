fab3d0833ef9877755421445eb886827c02c25bf
Elena Bychenkova <elena.bychenkova@gmail>
2022-06-23 23:26:54 -0700

    feat: config webpack

--- webpack-starter/src/js/index.js
+++ webpack-starter/src/js/index.js
@@ -1,1 +1,7 @@
+import "../styles/main.scss";
+import happy from "../assets/images/happy.jpg";
+
+const happyImg = document.getElementById("happyImg");
+happyImg.src = happy;
+
 console.log("Hello here!");
