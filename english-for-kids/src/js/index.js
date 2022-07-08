d4f3d12baf0680ca3779a03ad75785f240a28ced
Elena Bychenkova <elena.bychenkova@gmail>
2022-07-07 17:44:20 -0700

    feat: add eslint, prettier, normalize.css

--- english-for-kids/src/js/index.js
+++ english-for-kids/src/js/index.js
@@ -1,25 +1,24 @@
-import "../styles/normalize.css";
-import "../styles/main.scss";
-import styles from "../styles/main.scss";
-import happy from "../assets/images/happy.jpg";
+import 'normalize.css';
+import '../styles/main.scss';
+import styles from '../styles/app.module.scss';
+import happy from '../assets/images/happy.jpg';
 
-const APP_CONTAINER = document.getElementById("app-container");
+const APP_CONTAINER = document.getElementById('app-container');
 const DOC_FRAGMENT = document.createDocumentFragment();
 
-console.log(APP_CONTAINER);
+// console.log(APP_CONTAINER);
 
-const title = document.createElement("h1");
-title.innerHTML = "English for Kids";
+const title = document.createElement('h1');
+title.innerHTML = 'English for Kids';
 DOC_FRAGMENT.appendChild(title);
 
-const happyChildrenImg = document.createElement("img");
+const happyChildrenImg = document.createElement('img');
 happyChildrenImg.src = happy;
 DOC_FRAGMENT.appendChild(happyChildrenImg);
 
-const btn = document.createElement("button");
-// btn.classList.add("btn");
+const btn = document.createElement('button');
 btn.classList.add(styles.btn);
-btn.innerHTML = "Start";
+btn.innerHTML = 'Start';
 DOC_FRAGMENT.appendChild(btn);
 
 APP_CONTAINER.appendChild(DOC_FRAGMENT);
--- english-for-kids/webpack.config.js
+++ english-for-kids/webpack.config.js
@@ -1,68 +1,82 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-	mode: "development",
-	entry: {
-		bundle: path.resolve(__dirname, "src/js/index.js"),
-	},
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "[name][contenthash].js",
-		clean: true,
-		assetModuleFilename: "[name][ext]",
-	},
-	devServer: {
-		static: {
-			directory: path.resolve(__dirname, "dist"),
-		},
-		port: 3000,
-		open: true,
-		hot: true,
-		compress: true,
-		historyApiFallback: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.scss$/,
-				// use: ["style-loader", "css-loader", "sass-loader"],
-				use: [
-					"style-loader",
-					{
-						loader: "css-loader",
-						options: {
-							modules: true,
-						},
-					},
-					"sass-loader",
-				],
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["@babel/preset-env"],
-					},
-				},
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: "asset/resource",
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: "English for Kids App",
-			filename: "index.html",
-			template: "src/index.html",
-		}),
-	],
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/js/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.module\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:5]',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /\.module\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new ESLintPlugin({
+      fix: true,
+      lintDirtyModulesOnly: true,
+    }),
+    new HtmlWebpackPlugin({
+      title: 'English for Kids App',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.scss'],
+  },
 };
