85eae7efdbe0a10f8bb841ff08d19f68ab747899
Elena Bychenkova <elena.bychenkova@gmail>
2022-07-10 18:48:02 -0700

    feat: add fontawesome

--- english-for-kids/src/js/index.js
+++ english-for-kids/src/js/index.js
@@ -3,6 +3,9 @@ import 'Styles/main';
 import styles from 'Styles/app.module';
 import happy from 'Images/happy.jpg';
 
+import { faCirclePlay } from '@fortawesome/free-regular-svg-icons';
+import { icon } from '@fortawesome/fontawesome-svg-core';
+
 const APP_CONTAINER = document.getElementById('app-container');
 const DOC_FRAGMENT = document.createDocumentFragment();
 
@@ -10,15 +13,19 @@ const title = document.createElement('h1');
 title.innerHTML = 'English for Kids';
 DOC_FRAGMENT.appendChild(title);
 
-console.log(APP_CONTAINER);
-
 const happyChildrenImg = document.createElement('img');
 happyChildrenImg.src = happy;
 DOC_FRAGMENT.appendChild(happyChildrenImg);
 
 const btn = document.createElement('button');
 btn.classList.add(styles.btn);
-btn.innerHTML = 'Start';
+btn.innerHTML = `${
+  icon(faCirclePlay, {
+    transform: {
+      size: 24,
+    },
+  }).html
+}&nbsp;&nbsp;&nbsp;Start`;
 DOC_FRAGMENT.appendChild(btn);
 
 APP_CONTAINER.appendChild(DOC_FRAGMENT);
