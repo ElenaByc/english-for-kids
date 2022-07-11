f68c52b8d2bc1a584d3bf5a7ff7cdb878a9e7fbb
Elena Bychenkova <elena.bychenkova@gmail>
2022-07-11 16:53:38 -0700

    add app mode switcher

--- english-for-kids/src/js/header.js
+++ english-for-kids/src/js/header.js
@@ -3,6 +3,34 @@ import styles from 'Styles/header.module';
 function createrSwitcher() {
   const switcher = document.createElement('label');
   switcher.classList.add(styles.switcher);
+  const input = document.createElement('input');
+  input.setAttribute('type', 'checkbox');
+  input.classList.add(styles.input);
+  switcher.append(input);
+  const trainSpan = document.createElement('span');
+  trainSpan.innerText = 'Train';
+  trainSpan.classList.add(styles.label);
+  trainSpan.classList.add(styles.train);
+  switcher.append(trainSpan);
+  const playSpan = document.createElement('span');
+  playSpan.innerText = 'Play';
+  playSpan.classList.add(styles.label);
+  playSpan.classList.add(styles.play);
+  switcher.append(playSpan);
+  const handleSpan = document.createElement('span');
+  handleSpan.classList.add(styles.handle);
+  switcher.append(handleSpan);
+  switcher.addEventListener('click', () => {
+    const body = document.getElementsByTagName('body')[0];
+    console.log(body.classList);
+    if (body.classList.contains('play')) {
+      console.log('remove class PLAY');
+      body.classList.remove('play');
+    } else {
+      console.log('add class PLAY');
+      body.classList.add('play');
+    }
+  });
   return switcher;
 }
 
